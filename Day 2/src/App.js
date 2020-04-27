422b280ec87556f37fdd99a6172abfe62a55d374
Alex Bloom <lehanbloom@gmail.com>
2020-04-27 14:35:48 +0300

    Day 2. Ex 2. Use mounting and set to display new text on page after initial rendering.

--- Day 2/src/App.js
+++ Day 2/src/App.js
@@ -1,16 +1,10 @@
 import React from "react";
-import List from "./components/List";
-import user_data from "./data/user-mock-data.json";
+import Mount from './components/Mount'
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./css/App.css";
 
 function App() {
-  return (
-    <div>
-      <h1>User Data</h1>
-      <List list_data={user_data}></List>
-    </div>
-  );
+    return <Mount></Mount>
 }
 
 export default App;
